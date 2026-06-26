ed28bcdca85422a4a2a6aa6023429c9391439eec
ShawnMat <shawn.daniel1919@gmail.com>
2026-06-26 18:40:03 +0530

    Created Admin Dashboard and Working on token based system

--- scripts/AdminDashboard.js
+++ scripts/AdminDashboard.js
@@ -1,105 +1,148 @@
 const API = "http://localhost:3000"
-// import { appointmentAPI } from "./api.js";
+const today = new Date();
+const now = today.toISOString().split('T')[0];
+let token = 0;
+// let tokenLimit = 20;
+console.log(now)
+let sampleDataValue = "";
 
-// $(document).ready(()=>{
-//     console.log("webapge ready");
-// })
-
-async function addAppointment(){
-    try{
-
-        $('#patientnumber').on("input blur",function(){
-            const value = $(this).val();
-            const numberRegex = /^[6-9]\d{9}$/
-            if(numberRegex.test(value)){
-                $('#numberErrorMsg').text("Correct")
-            }
-            else{
-                $('#numberErrorMsg').text("")
-            }
-        })
-
-        // console.log(new Date());
-        
-
-
-        const patient_name = $('#patientname').val().trim()
-        const patient_age = $('#patientage').val().trim()
-        const patient_gender = $('#gender').val().trim()
-        const patient_number = $('#patientnumber').val().trim()
-        const reason = $('#reason').val().trim()
-        const assigned_doctor = $('#doctors').val().trim()
-        const appointment_date = $('#appointedDate').val().trim()
-        const appointment_time = $('#time').val().trim()
-        
-        const status = "Appointed";
-        // console.log(patient_gender,assigned_doctor,appointment_date,appointment_time);
-        const appointment_data = {patient_name,patient_age,patient_gender,patient_number,assigned_doctor,reason,appointment_date,appointment_time,status}
-
-        
-        await fetch(`${API}/appointments`,{
-            method: "POST",
-            headers: {
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(appointment_data)
-        })
-        const modal = bootstrap.Modal.getInstance(document.getElementById("createAppointmentModal"))
-        modal.hide()
-        getAppointments()
-    }
-    catch(e){
-        console.error(e)
-    }
-}
+// Get Appointments
 
 async function getAppointments(){
     const response = await fetch(`${API}/appointments`)
     const data =await response.json()
     console.log(data);
-    
     showAppointments(data)
 }
+
+// Display Appointments
 
 function showAppointments(data){
     // e.preventDefault()
     // $('#workspace').text(" ")
-    data.forEach(appointments=>
-        $('#table-content').append(`
-                        <tr>
-                            <td>1</td>
-                            <td>${appointments.patient_name}</td>
-                            <td>${appointments.reason}</td>
-                            <td>${appointments.assigned_doctor}</td>
-                            <!-- <td>25th June 2026</td> -->
-                            <td>${appointments.appointment_date}</td>
-                            <td>${appointments.appointment_time}</td>
-                            <!-- <td>${appointments.status}</td> -->
-                            <td><span class="status">${appointments.status}</span></td>
-                            <td>
-                                <button class="btn">
-                                    <i class="bi bi-pencil-square"></i>
-                                </button>
-                                <button class="btn">
-                                    <i class="bi bi-trash"></i>
-                                </button>
-                            </td>
-                        </tr>
-            `)
-    )
+    try{
+        console.log(data);
+    
+        data.forEach(appointments=>{
+            // allAppointments.push(appointments)
+            if(appointments.status == "Requested" && appointments.isDeleted === false){
+                console.log(appointments.id);
+                
+                $('#tableContent').append(`
+                            <tr>
+                                <td>1</td>
+                                <td>${appointments.PatientName}</td>
+                                <td>${appointments.PatientAge}</td>
+                                <td>${appointments.PatientGender}</td>
+                                <td>${appointments.reason}</td>
+                                <td>${appointments.assigned_doctor}</td>
+                                <!-- <td>25th June 2026</td> -->
+                                <td>${appointments.appointment_date}</td>
+                                <td>${appointments.appointment_time}</td>
+                                <!-- <td>${appointments.status}</td> -->
+                                <td><span class="status">${appointments.status}</span></td>
+                                <td>
+                                    <button id="checkBtn" class="btn" onclick = "approvedAppointments('${appointments.id}')" >
+                                        <i class="bi bi-check2-circle"></i>
+                                    </button>
+                                    <button id="rejectBtn" class="btn" onclick="rejectAppointments('${appointments.id}')">
+                                        <i class="bi bi-x-circle"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                    `)
+                    console.log(appointments);
+                    
+            }}
+        )
+        // counters();
+    }
+    catch(e){
+        console.log(e)
+    }
+    
 }
 
 
+async function rejectAppointments(id){
+    const task = await fetch(`${API}/appointments/${id}`)
+    const data = await task.json()
+
+    const modal = new bootstrap.Modal(document.getElementById("rejectAppointmentModal"))
+    modal.show()
+}
+
+async function doctorToken(doc){
+    // setTimeout(()=>{
+    //     console.log(data);
+    // },10000)
+   
+    // sampleDataValue = data;
+    const response = await fetch(`${API}/doctors?doctor_name=${doc}`)
+    const doctorData = await response.json()
+
+    const doctorTokens = doctorData.Tokens
+    if(doctorTokens < 1){
+        console.log("Token Limit Reached! Cannot assign anymore patiens to this Doctor");   
+    }
+    else{
+        doctorTokens -= 1
+        await fetch(`${API}/doctors?doctor_name=doc`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(
+                {
+                    "Tokens" : doctorTokens
+                }
+            )
+        })
+
+    }
+    
+    
+}
+// console.log(sampleDataValue);
 
 
+async function approvedAppointments(id){
+    const task = await fetch(`${API}/appointments/${id}`)
+    const data = await task.json()
+    // console.log(data)
+    // doctorToken(id)
+    token +=1
 
+    // CAN I CHECK THE CONDITION WHERE THE TOKEN EXCEEDS 20 COUNT??
 
-
-
-
-$('#createBtn').click(function(e){
-    // e.preventDefault();
-    addAppointment();
-})
+    if(data.assigned_doctor === "AsokKumar"){
+        let doc = data.assigned_doctor
+        doctorToken(doc)
+    }
+    else if(data.assigned_doctor === "DavidJames"){
+        let doc = data.assigned_doctor
+        doctorToken(doc)
+    }
+    else if(data.assigned_doctor === "SophiaPrince"){
+        let doc = data.assigned_doctor
+        doctorToken(doc)
+    }
+    await fetch(`${API}/appointments/${id}`,{
+                method: "PATCH",
+                headers: {
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify(
+                    {
+                        "status" : "Appointed",
+                        "TokenNumber" : token
+                    }
+                )
+            })
+    // data.status = "Appointed"
+    // console.log(data.status)
+    
+}
 
 getAppointments()
+
--- scripts/Dashboard.js
+++ scripts/Dashboard.js
@@ -2,22 +2,28 @@ const API = "http://localhost:3000"
 // import { appointmentAPI } from "./api.js";
 const today = new Date();
 const now = today.toISOString().split('T')[0];
+// sampleDate = "2026/"
+console.log(now)
+// console.log(today.toLocaleDateString())
+let editID = null;
+let allAppointments=[]
+// $('#search').input("input blur",
+//     allAppointments.forEach(appointment=>{
+//         if($(this).val()== appointment.assigned_doctor)
+//     })
+// )
+
+
+
 
 async function addAppointment(){
     try{
-        // console.log(new Date());
-
-        // const patient_name = $('#patientname').val().trim()
-        // const patient_age = $('#patientage').val().trim()
-        // const patient_gender = $('#gender').val().trim()
-        // const patient_number = $('#patientnumber').val().trim()
         const reason = $('#reason').val().trim()
         const assigned_doctor = $('#doctors').val().trim()
         const appointment_date = $('#appointedDate').val().trim()
         const appointment_time = $('#time').val().trim()
-        
         const status = "Requested";
-        
+        const isDeleted = false
         // console.log(patient_gender,assigned_doctor,appointment_date,appointment_time);
         if(!reason || !assigned_doctor || !appointment_date || !appointment_time){
             console.log("Input is required");   
@@ -25,16 +31,16 @@ async function addAppointment(){
                 console.log("Input Reason Required");
                 $('#reasonError').text("Input Required").addClass('errorMsg')
             }
-            else if (!assigned_doctor){
+            if (!assigned_doctor){
                 console.log("Input assigned Doctor Required");
                 $('#doctorError').text("Input Required").addClass('errorMsg')
             }
             // for date validation, we need to add the time slot of the current date (NEED TO DO)
-            else if (!appointment_date  ){
+            if (!appointment_date){
                 console.log("Input Appointed Date cannot be before today's date");
                 $('#dateError').text("Input Required").addClass('errorMsg')
             }
-            else if (!appointment_time){
+            if (!appointment_time){
                 console.log("Input appointment Time Required");
                 $('#timeError').text("Input Required").addClass('errorMsg')
             }
@@ -42,7 +48,7 @@ async function addAppointment(){
         }
         else{
         
-            const appointment_data = {assigned_doctor,reason,appointment_date,appointment_time,status}
+            const appointment_data = {assigned_doctor,reason,appointment_date,appointment_time,status,isDeleted}
 
             
             await fetch(`${API}/appointments`,{
@@ -65,49 +71,186 @@ async function addAppointment(){
 async function getAppointments(){
     const response = await fetch(`${API}/appointments`)
     const data =await response.json()
-    // console.log(data);
-    
     showAppointments(data)
 }
 
 function showAppointments(data){
     // e.preventDefault()
     // $('#workspace').text(" ")
-    data.forEach(appointments=>
-        $('#table-content').append(`
-                        <tr>
-                            <td>1</td>
-                            <td>${appointments.reason}</td>
-                            <td>${appointments.assigned_doctor}</td>
-                            <!-- <td>25th June 2026</td> -->
-                            <td>${appointments.appointment_date}</td>
-                            <td>${appointments.appointment_time}</td>
-                            <!-- <td>${appointments.status}</td> -->
-                            <td><span class="status">${appointments.status}</span></td>
-                            <td>
-                                <button class="btn">
-                                    <i class="bi bi-pencil-square"></i>
-                                </button>
-                                <button class="btn">
-                                    <i class="bi bi-trash"></i>
-                                </button>
-                            </td>
-                        </tr>
-            `)
-    )
+        data.forEach(appointments=>{
+            // allAppointments.push(appointments)
+            if(appointments.isDeleted == false){
+                $('#table-content').append(`
+                                <tr>
+                                    <td>1</td>
+                                    <td>${appointments.reason}</td>
+                                    <td>${appointments.assigned_doctor}</td>
+                                    <!-- <td>25th June 2026</td> -->
+                                    <td>${appointments.appointment_date}</td>
+                                    <td>${appointments.appointment_time}</td>
+                                    <!-- <td>${appointments.status}</td> -->
+                                    <td><span class="status">${appointments.status}</span></td>
+                                    <td>Not Assigned</td>
+                                    <td>
+                                        <button id="editBtn" class="btn" onclick="editAppointments('${appointments.id}')">
+                                            <i class="bi bi-pencil-square"></i>
+                                        </button>
+                                        <button id="deleteBtn" class="btn" onclick="deleteAppointments('${appointments.id}')">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                    `)
+            }}
+        )
+        counters();
+}
+
+// counters function
+async function counters(){
+    const task = await fetch(`${API}/appointments`)
+    const data = await task.json()  
+    console.log(data);
+     
+    const visitCount = data.filter(appointments=>
+        appointments.status === "Visited" && appointments.isDeleted === false
+    ).length
+    const appointCount = data.filter(appointments=>
+        appointments.status == "Appointed" && appointments.isDeleted === false
+    ).length
+    const reqCount = data.filter(appointments=>
+        appointments.status == "Requested" && appointments.isDeleted === false
+    ).length
+    const missCount = data.filter(appointments=>
+        appointments.status == "Missed" && appointments.isDeleted === false
+    ).length
+
+    $('#visitedCount').text(visitCount)
+    $('#appointedCount').text(appointCount)
+    $('#requestedCount').text(reqCount)
+    $('#missedCount').text(missCount)
+    
 }
 
 
 
 
 
+async function deleteAppointments(id){
+    const task = await fetch(`${API}/appointments/${id}`)
+    const data = await task.json()
+    await fetch(`${API}/appointments/${id}`,{
+        method: "PATCH",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+            "isDeleted" : true
+        }
+        )
+    })
+}
+
+async function editAppointments(id){
+    editID = id;
+    const task = await fetch(`${API}/appointments/${id}`)
+    const data = await task.json()
+
+    $('#editReason').val(data.reason)
+    $('#editDoctors').val(data.assigned_doctor)
+    $('#editAppointedDate').val(data.appointment_date)
+    $('#editTime').val(data.appointment_time)
+
+
+    const modal = new bootstrap.Modal(document.getElementById("editAppointmentModal"))
+    modal.show()
+}
 
 
 
+
+async function updateAppointments(){
+    const reason = $('#editReason').val().trim()
+    const assigned_doctor = $('#editDoctors').val().trim()
+    const appointment_date = $('#editAppointedDate').val().trim()
+    const appointment_time = $('#editTime').val().trim()
+
+
+    const appointmentData = {reason,assigned_doctor,appointment_date,appointment_time}
+    await fetch(`${API}/appointments/${editID}`,{
+        method: "PATCH",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(appointmentData)
+    })
+}
+
+
+async function filterAppointments(){
+    const task = await fetch(`${API}/appointments`)
+    const data = await task.json()  
+    console.log(data);
+    data.forEach(values=>{
+        
+        if(values.status === "Visited" && values.isDeleted === false){
+            showFilterAppointments(values)
+        }
+        else if(values.status === "Requested" && values.isDeleted === false){
+            showFilterAppointments(values)
+        }
+        else if(values.status === "Appointed" && values.isDeleted === false){
+            showFilterAppointments(values)
+        }
+        else if(values.status === "Missed" && values.isDeleted === false){
+            showFilterAppointments(values)
+        }
+    })
+
+    function showFilterAppointments(values){
+        $('#table-content').empty()
+        $('#table-content').append(`
+                                <tr>
+                                    <td>1</td>
+                                    <td>${values.reason}</td>
+                                    <td>${values.assigned_doctor}</td>
+                                    <!-- <td>25th June 2026</td> -->
+                                    <td>${values.appointment_date}</td>
+                                    <td>${values.appointment_time}</td>
+                                    <!-- <td>${values.status}</td> -->
+                                    <td><span class="status">${values.status}</span></td>
+                                    <td>
+                                        <button id="editBtn" class="btn" onclick="editAppointments('${values.id}')">
+                                            <i class="bi bi-pencil-square"></i>
+                                        </button>
+                                        <button id="deleteBtn" class="btn" onclick="deleteAppointments('${values.id}')">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+        `)
+    }    
+}
+
+
+$('#modalEditBtn').click(function(e){
+    updateAppointments();
+})
 
 $('#createBtn').click(function(e){
     // e.preventDefault();
     addAppointment();
 })
+$('#appointedDate').attr('min',now)
+
+// $('#filter').click(()=>{
+//     filterAppointments()
+// })
+
+$('#filter').click(()=>{
+    $('#filter-contents').addClass('show')
+})
+
+
 
 getAppointments()
